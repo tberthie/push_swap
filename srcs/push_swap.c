7519d3779eb5056614c549366b9bff00ffd621a7
Thibault BERTHIER <tberthie@e2r7p7.42.fr>
2017-07-12 16:41:13 +0200

    Fixed overflow

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: tberthie <tberthie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/20 22:20:00 by tberthie          #+#    #+#             */
-/*   Updated: 2017/02/22 14:19:46 by tberthie         ###   ########.fr       */
+/*   Updated: 2017/07/12 16:40:39 by tberthie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 static void		solve(t_stack *a, t_stack *b, char flag)
 {
 	//do stuff
-
+	push(a, b);
 	if (flag)
 		display(a, b);
 	if (check(a, b))
@@ -29,28 +29,28 @@ static void		solve(t_stack *a, t_stack *b, char flag)
 
 int				main(int ac, char **av)
 {
-	t_stack		a;
-	t_stack		b;
+	t_stack		*a;
+	t_stack		*b;
 	char		flag;
 
 	if (ac <= 1)
 		return (0);
 	av += (flag = !ft_strcmp(av[1], "-v"));
-	a.stack = (int*)ft_m(sizeof(int) * (unsigned int)(ac - 1));
-	b.stack = (int*)ft_m(sizeof(int) * (unsigned int)(ac - 1));
-	a.size = 0;
-	b.size = 0;
+	a = (t_stack*)ft_memalloc(sizeof(t_stack));
+	b = (t_stack*)ft_memalloc(sizeof(t_stack));
+	a->stack = (int*)ft_m(sizeof(int) * (unsigned int)(ac - 1));
+	b->stack = (int*)ft_m(sizeof(int) * (unsigned int)(ac - 1));
 	while (*++av)
 	{
-		if (!check_nbr(*av, &a.stack[a.size], &a))
+		if (!check_nbr(*av, &a->stack[a->size], a))
 		{
 			write(2, "\x1b[31mError\n", 11);
 			return (0);
 		}
-		a.size++;
+		a->size++;
 	}
 	if (flag)
-		display(&a, &b);
-	solve(&a, &b, flag);
+		display(a, b);
+	solve(a, b, flag);
 	return (0);
 }
